df0e3d8b971bc7a04ef9c9f64644c18f3848502e
Thanh Pham <thanhpham091790@gmail.com>
2020-12-20 15:33:38 -0700

    Style for contact section

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
-function openTab(event, tabName){
+function openTab(event, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
-    for(i=0;i<tabcontent.length;i++){
+    for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
-    for(i=0;i<tablinks.length;i++){
+    for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
